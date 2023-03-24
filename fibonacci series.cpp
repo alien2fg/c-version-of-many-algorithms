c56b91e5bac9fa3b897803322d366d79ab07bc90
Filip Gnojek <94650570+alien2fg@users.noreply.github.com>
2023-03-24 09:22:49 +0100

    Update fibonacci series.cpp

--- fibonacci series.cpp
+++ fibonacci series.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
 
+void fibonacci(int size)
+{    
+     long long a = 0, b = 1;
+  
+     for(int i=0;i<size;i++)
+     {
+            cout<<b<<" ";
+            b =b + a; 
+            a =b - a; 
+     }     
+}
+
+int main()
+{
+    int size;
+    
+    cout<<"Give the number of elements of the Fibonacci sequence: ";
+    cin>>size;
+    
+    fibonacci(size);
+    
+    return 0;
+}
